72eadcd2785a85571af2a6d93ba6fd899e9eb8f3
Anshul Kumar <anshul2004ak@gmail.com>
2025-12-07 14:11:42 +0530

    fix server listen config for render

--- server/src/index.js
+++ server/src/index.js
@@ -54,6 +54,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
